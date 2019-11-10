e4ce9f1044f28f2dd8a3619e83a28aabdc0a9ab0
fermat93 <jinzengg@gmail.com>
2019-11-11 01:00:43 +0800

    add

--- sql/外协导数.sql
+++ sql/外协导数.sql
@@ -10,6 +10,6 @@ end as 'job_title',
 `employment_date` ,`employment_days` ,`final_audit_num` ,`shift_id` ,date_format(CONVERT_TZ(`created_at`, '+00:00', '+07:00'),'%Y-%m-%d %H:%i:%s') as created_at
 FROM `backyard_pro`.`hr_staff_outsourcing`
 WHERE `status` =2
-and  `employment_date` >'2019-11-09'
+and  `employment_date` >'2019-11-10'
 ORDER BY `created_at` 
 
